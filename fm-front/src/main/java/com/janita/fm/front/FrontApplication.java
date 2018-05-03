35759126d7757dcf57d06892e4730a133b5af0ef
hspcadmin <880902>
2018-05-03 09:56:17 +0800

    init

---
+++ fm-front/src/main/java/com/janita/fm/front/FrontApplication.java
@@ -0,0 +1,16 @@
+package com.janita.fm.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2018/5/3.
+ * Desc:
+ */
+@SpringBootApplication
+public class FrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontApplication.class, args);
+    }
+}
